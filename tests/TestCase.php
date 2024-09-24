e9373a8c5e669d9b8b191ec46be24e1fd30e6864
Luiz F. Gonçalves <luizfelipe.gsanches@hotmail.com>
2024-09-24 14:40:08 -0300

    Config cache for testing environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,29 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Redis;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (app()->environment('testing')) {
+            self::clearRedisKeys([
+                'account_balance:*'
+            ]);
+        }
+    }
+
+    private static function clearRedisKeys(array $locks_keys)
+    {
+        $redisPrefix = config('database.redis.options.prefix');
+        foreach ($locks_keys as $lock_key_pattern) {
+            foreach (Redis::keys($lock_key_pattern) as $key) {
+                $rawKey = str($key)->replace($redisPrefix, '')->toString();
+                Redis::del($rawKey);
+            }
+        }
+    }
 }
